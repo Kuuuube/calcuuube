fe899ba00f4c38eeb2525463b34c8e9381dddf4b
kuuuube <hexagonisalie@gmail.com>
2024-07-20 18:07:33 -0400

    Fix equals button

--- src/gui.rs
+++ src/gui.rs
@@ -148,7 +148,7 @@ impl eframe::App for CalcuuubeGui {
                         make_button(self, ui, "0");
                         make_button(self, ui, ".");
                         make_backspace_button(self, ui);
-                        make_button(self, ui, "=");
+                        make_equals_button(self, ui);
                         ui.end_row();
                     });
             });
@@ -178,6 +178,18 @@ fn make_backspace_button(calcuuube_gui: &mut CalcuuubeGui, ui: &mut egui::Ui) {
     if calcuuube_gui.clicked && new_button.is_pointer_button_down_on() {
         calcuuube_gui.clicked = false;
         calcuuube_gui.input_text.pop();
+        calculate_result(calcuuube_gui);
+    }
+}
+
+fn make_equals_button(calcuuube_gui: &mut CalcuuubeGui, ui: &mut egui::Ui) {
+    let new_button = ui.add_sized(
+        ui.available_size(),
+        egui::Button::new(egui::RichText::new("=")),
+    );
+
+    if calcuuube_gui.clicked && new_button.is_pointer_button_down_on() {
+        calcuuube_gui.clicked = false;
         calculate_result(calcuuube_gui);
     }
 }
