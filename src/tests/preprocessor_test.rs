c2d5d8be44839c1ca65f04ffeb9e9f08d4fecdc5
kuuuube <hexagonisalie@gmail.com>
2024-07-20 18:30:08 -0400

    Add more preprocessor tests

--- src/tests/preprocessor_test.rs
+++ src/tests/preprocessor_test.rs
@@ -3,6 +3,9 @@ use crate::preprocessor::preprocessor;
 
 #[test]
 pub fn test_preprocessor() {
+    assert!(preprocessor("√2") == "√(2)".to_owned());
+    assert!(preprocessor("√22") == "√(22)".to_owned());
+    assert!(preprocessor("√123123123.456456") == "√(123123123.456456)".to_owned());
     assert!(preprocessor("√2*2") == "√(2)*2".to_owned());
     assert!(preprocessor("√2+2") == "√(2)+2".to_owned());
     assert!(preprocessor("√2-2") == "√(2)-2".to_owned());
