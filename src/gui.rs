19bcc8271dceadceb23ea97f7c1607e54d35e485
kuuuube <hexagonisalie@gmail.com>
2024-07-21 17:24:18 -0400

    Switch percent button to log button

--- src/gui.rs
+++ src/gui.rs
@@ -214,7 +214,7 @@ impl eframe::App for CalcuuubeGui {
                     )
                     .show(ui, |ui| {
                         make_button(self, ui, "√");
-                        make_button(self, ui, "%");
+                        make_button(self, ui, "log(");
                         make_button(self, ui, "^");
                         make_button(self, ui, "x²");
                         ui.end_row();
