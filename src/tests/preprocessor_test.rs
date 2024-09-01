f06bd4573e0a1751cade0b4609e59a9577880528
kuuuube <hexagonisalie@gmail.com>
2024-09-01 09:28:36 -0400

    Add comma removal tests

--- src/tests/preprocessor_test.rs
+++ src/tests/preprocessor_test.rs
@@ -27,3 +27,10 @@ pub fn test_preprocessor_ending_parentheses() {
     assert!(preprocessor("log(5") == "log(5)".to_owned());
     assert!(preprocessor("√(2") == "√(2)".to_owned());
 }
+
+#[test]
+pub fn test_preprocessor_remove_commas() {
+    assert!(preprocessor("100,000") == "100000".to_owned());
+    assert!(preprocessor("10,000") == "10000".to_owned());
+    assert!(preprocessor(",") == "".to_owned());
+}
