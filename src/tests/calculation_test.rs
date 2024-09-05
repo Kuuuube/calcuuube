19b66539b05ec0564f5b9a1c8f340e2e4b4e4e75
kuuuube <hexagonisalie@gmail.com>
2024-09-04 23:17:13 -0400

    Fix sqrt 2 test

--- src/tests/calculation_test.rs
+++ src/tests/calculation_test.rs
@@ -9,7 +9,7 @@ pub fn test_calculation_outputs() {
     assert!(calculate_string_to_string("5393000*500", &mut parser_context).unwrap() == "2696500000".to_owned());
     assert!(calculate_string_to_string("1/3", &mut parser_context).unwrap() == "0.3333333333333333".to_owned());
     assert!(calculate_string_to_string("0.1+0.2", &mut parser_context).unwrap() == "0.3".to_owned());
-    assert!(calculate_string_to_string("√2", &mut parser_context).unwrap() == "1.414213562".to_owned());
+    assert!(calculate_string_to_string("√2", &mut parser_context).unwrap() == "1.4142135624".to_owned());
     assert!(calculate_string_to_string("10*1000000000000000", &mut parser_context).unwrap() == "10000000000000000".to_owned());
     assert!(calculate_string_to_string("10*1000000000000000000000000000000000000000000000000000", &mut parser_context).unwrap() == "10^52".to_owned());
     assert!(calculate_string_to_string("1234567890", &mut parser_context).unwrap() == "1234567890".to_owned());
