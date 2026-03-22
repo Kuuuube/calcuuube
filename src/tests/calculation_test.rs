33b0cfb5ecf8f8064cd31a86a37773e66f374725
kuuuube <hexagonisalie@gmail.com>
2026-03-22 14:50:50 -0400

    Fix tests

--- src/tests/calculation_test.rs
+++ src/tests/calculation_test.rs
@@ -7,14 +7,15 @@ pub fn test_calculation_outputs() {
     let mut parser_context = fend_core::Context::new();
     assert!(calculate_string_to_string("", &mut parser_context).unwrap() == "".to_owned());
     assert!(calculate_string_to_string("5393000*500", &mut parser_context).unwrap() == "2696500000".to_owned());
-    assert!(calculate_string_to_string("1/3", &mut parser_context).unwrap() == "0.3333333333333333".to_owned());
+    assert!(calculate_string_to_string("1/3", &mut parser_context).unwrap() == "0.3333333333".to_owned());
     assert!(calculate_string_to_string("0.1+0.2", &mut parser_context).unwrap() == "0.3".to_owned());
     assert!(calculate_string_to_string("√2", &mut parser_context).unwrap() == "1.4142135624".to_owned());
+    assert!(calculate_string_to_string("√", &mut parser_context) == None);
     assert!(calculate_string_to_string("10*1000000000000000", &mut parser_context).unwrap() == "10000000000000000".to_owned());
-    assert!(calculate_string_to_string("10*1000000000000000000000000000000000000000000000000000", &mut parser_context).unwrap() == "10^52".to_owned());
+    assert!(calculate_string_to_string("10*1000000000000000000000000000000000000000000000000000", &mut parser_context).unwrap() == "10000000000000000000000000000000000000000000000000000".to_owned());
     assert!(calculate_string_to_string("1234567890", &mut parser_context).unwrap() == "1234567890".to_owned());
     assert!(calculate_string_to_string("0.0000000000000000000000000000000000000000000001/5", &mut parser_context).unwrap() == "0.00000000000000000000000000000000000000000000002".to_owned());
-    assert!(calculate_string_to_string("log₁₀(9)", &mut parser_context).unwrap() == "0.9542425094393249".to_owned());
+    assert!(calculate_string_to_string("log₁₀(9)", &mut parser_context).unwrap() == "0.9542425094".to_owned());
     assert!(calculate_string_to_string("2.7", &mut parser_context).unwrap() == "2.7".to_owned());
     assert!(calculate_string_to_string("2.1", &mut parser_context).unwrap() == "2.1".to_owned());
     assert!(calculate_string_to_string("0.1+0.2", &mut parser_context).unwrap() == "0.3".to_owned());
--- src/tests/preprocessor_test.rs
+++ src/tests/preprocessor_test.rs
@@ -3,34 +3,38 @@ use crate::preprocessor::preprocessor;
 
 #[test]
 pub fn test_preprocessor_sqrts() {
-    assert!(preprocessor("√2") == "√(2)".to_owned());
-    assert!(preprocessor("√22") == "√(22)".to_owned());
-    assert!(preprocessor("√123123123.456456") == "√(123123123.456456)".to_owned());
-    assert!(preprocessor("√2*2") == "√(2)*2".to_owned());
-    assert!(preprocessor("√2+2") == "√(2)+2".to_owned());
-    assert!(preprocessor("√2-2") == "√(2)-2".to_owned());
-    assert!(preprocessor("√2/2") == "√(2)/2".to_owned());
-    assert!(preprocessor("√2^2") == "√(2)^2".to_owned());
-    assert!(preprocessor("√(2*2)") == "√(2*2)".to_owned());
-    assert!(preprocessor("√2*2√2*2") == "√(2)*2√(2)*2".to_owned());
-    assert!(preprocessor("√2√2*2") == "√(2)√2*2".to_owned());
-    assert!(preprocessor("√-1") == "√(-1)".to_owned());
-    assert!(preprocessor("√-1*2") == "√(-1)*2".to_owned());
+    assert!(preprocessor("√") == "@plain_number √".to_owned());
+    assert!(preprocessor("√2") == "@plain_number sqrt(2)".to_owned());
+    assert!(preprocessor("√22") == "@plain_number sqrt(22)".to_owned());
+    assert!(preprocessor("√123123123.456456") == "@plain_number sqrt(123123123.456456)".to_owned());
+    assert!(preprocessor("√2*2") == "@plain_number sqrt(2)*2".to_owned());
+    assert!(preprocessor("√2+2") == "@plain_number sqrt(2)+2".to_owned());
+    assert!(preprocessor("√2-2") == "@plain_number sqrt(2)-2".to_owned());
+    assert!(preprocessor("√2/2") == "@plain_number sqrt(2)/2".to_owned());
+    assert!(preprocessor("√2^2") == "@plain_number sqrt(2)^2".to_owned());
+    assert!(preprocessor("√(2*2)") == "@plain_number sqrt(2*2)".to_owned());
+    assert!(preprocessor("√2*2√2*2") == "@plain_number sqrt(2)*2sqrt(2)*2".to_owned());
+    assert!(preprocessor("√2√2*2") == "@plain_number sqrt(2)sqrt(2)*2".to_owned());
+    assert!(preprocessor("√-1") == "@plain_number sqrt(-1)".to_owned());
+    assert!(preprocessor("√-1*2") == "@plain_number sqrt(-1)*2".to_owned());
+    assert!(preprocessor("√(√(√(√(2))))") == "@plain_number sqrt(sqrt(sqrt(sqrt(2))))".to_owned());
 }
 
 #[test]
 pub fn test_preprocessor_ending_parentheses() {
-    assert!(preprocessor("(2*5") == "(2*5)".to_owned());
-    assert!(preprocessor("((2*5") == "((2*5))".to_owned());
-    assert!(preprocessor("2*5)") == "2*5)".to_owned());
-    assert!(preprocessor("((((") == "(((())))".to_owned());
-    assert!(preprocessor("log(5") == "log(5)".to_owned());
-    assert!(preprocessor("√(2") == "√(2)".to_owned());
+    dbg!(preprocessor("(2*5"));
+    assert!(preprocessor("(2*5") == "@plain_number (2*5)".to_owned());
+    assert!(preprocessor("((2*5") == "@plain_number ((2*5))".to_owned());
+    assert!(preprocessor("2*5)") == "@plain_number 2*5)".to_owned());
+    assert!(preprocessor("((((") == "@plain_number (((())))".to_owned());
+    assert!(preprocessor("log(5") == "@plain_number log(5)".to_owned());
+    assert!(preprocessor("√(2") == "@plain_number sqrt(2)".to_owned());
+    assert!(preprocessor("sqrt(2") == "@plain_number sqrt(2)".to_owned());
 }
 
 #[test]
 pub fn test_preprocessor_remove_commas() {
-    assert!(preprocessor("100,000") == "100000".to_owned());
-    assert!(preprocessor("10,000") == "10000".to_owned());
+    assert!(preprocessor("100,000") == "@plain_number 100000".to_owned());
+    assert!(preprocessor("10,000") == "@plain_number 10000".to_owned());
     assert!(preprocessor(",") == "".to_owned());
 }
